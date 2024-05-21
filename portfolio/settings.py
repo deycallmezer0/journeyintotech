fd82e20a79171bd46856bd97c855a1f371846978
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 13:03:09 -0400

    chore: Update ALLOWED_HOSTS to include "journeyinto.tech"

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b4nr*$q2ydvxb#^deji36c$))kl%c%9!5te8ak(5z#lzilq*!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["journeyinto.tech"]
 
 
 # Application definition

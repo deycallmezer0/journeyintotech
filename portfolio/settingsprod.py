4dad2c13d125109c5c959eb22560b76e15451fc6
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 13:19:53 -0400

    chore: Update STATIC_URL in settingsprod.py to fix static file serving issue

--- portfolio/settingsprod.py
+++ portfolio/settingsprod.py
@@ -92,13 +92,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/staticfiles')]
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-# Media files
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

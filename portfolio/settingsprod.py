114990e66c13def1d10bcd08c0860b9d15ebfdf7
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 13:21:40 -0400

    chore: Update STATIC_URL in settings.py and settingsprod.py to 'main/static/' for serving static files

--- portfolio/settingsprod.py
+++ portfolio/settingsprod.py
@@ -92,7 +92,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
+STATIC_URL = 'main/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

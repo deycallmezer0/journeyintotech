c4d0c9596907d62d2f5ca247d58c8111ec6e8f1b
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 08:59:38 -0400

    chore: Add required files for Vercel deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -110,10 +110,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

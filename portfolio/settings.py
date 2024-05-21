f192da2588bf3d7c614302ddf8c4819783cb9a5e
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 13:51:16 -0400

    chore: Update STATIC_URL in settings.py and settingsprod.py to use '\static' for serving static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -92,13 +92,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'main/static/'
+STATIC_URL = '\static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main\static')]
 
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '\media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- portfolio/settingsprod.py
+++ portfolio/settingsprod.py
@@ -92,12 +92,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'main/static/'
+
+STATIC_URL = '\static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main\static')]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '\media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

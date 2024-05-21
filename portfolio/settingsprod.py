f852eb6db72e56f557b55cd6a883fb5b6b2f4fd0
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 13:56:51 -0400

    chore: Update STATIC_URL in settings.py and settingsprod.py to use '/static/' for serving static files

--- portfolio/settingsprod.py
+++ portfolio/settingsprod.py
@@ -93,9 +93,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 
-STATIC_URL = '\static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main\static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/main/static')]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

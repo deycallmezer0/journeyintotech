786b57e43e35651829d59fa9bd58217b0cc402fd
Chris Holmes <crewloveovo@gmail.com>
2024-05-21 12:34:36 -0400

    chore: Update ALLOWED_HOSTS in settings.py and settingsprod.py

--- portfolio/settingsprod.py
+++ portfolio/settingsprod.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['journeyinto.tech']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 INSTALLED_APPS = [
